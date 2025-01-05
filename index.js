a21a52f925854d1a2bc66d65bf2e4fe585e6db3d
John Voloski <johnvoloski@cwi.com.br>
2025-01-05 13:48:05 -0300

    structure

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello NodeJS!');
+const express = require('express');
+const tracer = require('dd-trace').init();
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.status(200).send({
+    success: true,
+    message: 'Working',
+    version: '1.0.0',
+  });
+});
+
+app.listen(port);
+
+console.log(port)
